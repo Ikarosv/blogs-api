7e6dba2998e766b54309c3a0f246f730b09e572b
Ikarosv <ikarovieiraa@gmail.com>
2023-04-11 14:33:55 -0300

    ✨ feat: adiciona expires no token

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -1,6 +1,6 @@
 const { sign } = require('jsonwebtoken');
 
 module.exports = (payload) => {
-  const token = sign(JSON.stringify(payload), process.env.JWT_SECRET);
+  const token = sign(JSON.stringify(payload), process.env.JWT_SECRET, { expiresIn: '2d' });
   return token;
 };

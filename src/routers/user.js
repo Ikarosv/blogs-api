58e1ad3270c69e806eee5b4bedf7bed7958789ca
Ikarosv <ikarovieiraa@gmail.com>
2023-04-11 11:58:46 -0300

    ✨ feat: adiciona middlewares

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,8 +1,10 @@
 const { Router } = require('express');
 const { createNewUser } = require('../controllers/user');
+const validateNewUser = require('../middlewares/validateNewUser');
+const validateEmailAlreadyExists = require('../middlewares/validateEmailAlreadyExists');
 
 const userRouter = Router();
 
-userRouter.post('/', createNewUser);
+userRouter.post('/', validateNewUser, validateEmailAlreadyExists, createNewUser);
 
 module.exports = userRouter;

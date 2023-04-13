b4a12dff5322d8e632886db571b46a2b30854794
Ikarosv <ikarovieiraa@gmail.com>
2023-04-13 16:38:50 -0300

    ✨ feat: adiciona rota /categories

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const loginRouter = require('./routers/login');
 const userRouter = require('./routers/user');
+const categoriesRouter = require('./routers/categories');
 
 // ...
 
@@ -9,6 +10,7 @@ app.use(express.json());
 
 app.use('/login', loginRouter);
 app.use('/user', userRouter);
+app.use('/categories', categoriesRouter);
 
 // não remova ou mova esse endpoint
 app.get('/', (_request, response) => {
